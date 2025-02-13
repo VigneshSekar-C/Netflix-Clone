a3720ef6a1acaa536690112294ef62de002470b5
VigneshSekar-C <vigneshsekar1006@gmail.com>
2025-02-13 22:00:00 +0530

    Made Some Changes

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,32 +1,32 @@
-// import React, { useEffect, useRef } from 'react'
-// import './TitleCards.css'
-// import cards_data from '../../assets/cards/Cards_data'
+import React, { useEffect, useRef } from 'react'
+import './TitleCards.css'
+import cards_data from '../../assets/cards/Cards_data'
 
-// const TiltleCards = () => {
+const TiltleCards = () => {
 
-// const cardsRef = useRef();
+const cardsRef = useRef();
 
-// const handleWheel = (event) => {
-//     event.preventDefault();
-//     cardsRef.current.scrollLeft += event.deltaY;
-//   }
+const handleWheel = (event) => {
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+  }
 
-// useEffect(() => {
-//   cardsRef.current.addEventListener('wheel', handleWheel);
-// },[])
-//     return (
-//       <div className="title-cards">
-//         <h2>Popular on Netflix</h2>
-//         <div className="card-list" ref={cardsRef}>
-//           {cards_data.map((card, index) => {
-//             return <div className="card" key={index}>
-//               <img src={card.image} alt="" />
-//               <p>{card.name}</p>
-//             </div>
-//           })}
-//         </div>
-//       </div>
-//     )
-//   }
+useEffect(() => {
+  cardsRef.current.addEventListener('wheel', handleWheel);
+},[])
+    return (
+      <div className="title-cards">
+        <h2>Popular on Netflix</h2>
+        <div className="card-list" ref={cardsRef}>
+          {cards_data.map((card, index) => {
+            return <div className="card" key={index}>
+              <img src={card.image} alt="" />
+              {/* <p>{card.name}</p> */}
+            {/* </div>
+          })}
+        </div>
+      </div> */}
+    )
+  }
 
-//   export default TiltleCards
+  export default TiltleCards

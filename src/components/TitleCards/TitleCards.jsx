285521a373c427ac2245d8e5896c848d5bc81519
VigneshSekar-C <vigneshsekar1006@gmail.com>
2025-02-15 22:46:38 +0530

    Made some changes

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -17,14 +17,14 @@ useEffect(() => {
     return (
       <div className="title-cards">
         <h2>Popular on Netflix</h2>
-        <div className="card-list" ref={cardsRef}>
+        {/* <div className="card-list" ref={cardsRef}>
           {cards_data.map((card, index) => {
             return <div className="card" key={index}>
               <img src={card.image} alt="" />
               <p>{card.name}</p> */
             </div>
           })}
-        </div>
+        </div> */}
       </div>
     )
   }

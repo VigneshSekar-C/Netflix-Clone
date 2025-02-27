3ae81a401ffe311b1dad30a410db8fac9636e241
VigneshSekar-C <vigneshsekar1006@gmail.com>
2025-02-27 23:28:42 +0530

    Made some changes

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,10 +1,22 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 
 const TitleCards = ({title, category}) => {
 
-const cardsRef = useRef();
+  const [apiData, setApiData] = useState([]);
+
+  const cardsRef = useRef();
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYmI0Y2JhYWNhOWUzMWU2NTVmMTc0YTNhMWM5N2JkNyIsIm5iZiI6MTc0MDY3NDg4MS42Nywic3ViIjoiNjdjMDk3NDFhNmU1NTExMTlhMzVhNmUzIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.iaPGlZEfm_NgKc2_7Yk_uMPjIgiZfoH16fZeFKIDh9Q'
+    }
+  };
+
+
 
 const handleWheel = (event) => {
     event.preventDefault();
@@ -12,16 +24,20 @@ const handleWheel = (event) => {
   }
 
 useEffect(() => {
+  fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`, options)
+  .then(res => res.json())
+  .then(res => setApiData(res.results))
+  .catch(err => console.error(err));
   cardsRef.current.addEventListener('wheel', handleWheel);
 },[])
     return (
       <div className="title-cards">
         <h2>{title?title:"Popular on Netflix"}</h2>
         <div className="card-list" ref={cardsRef}>
-          {cards_data.map((card, index) => {
+          {apiData.map((card, index) => {
             return <div className="card" key={index}>
-              <img src={card.image} alt="" />
-              <p>{card.name}</p> 
+              <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt="" />
+              <p>{card.original_title}</p> 
             </div>
           })}
         </div> 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -29,10 +29,10 @@ const Home = () => {
         </div>
       </div>
       <div className="more-cards">
-      <TitleCards title={"Blockbuster Movies"}/>
-      <TitleCards title={"Only On Netflix"}/>
-      <TitleCards title={"Upcoming"}/>
-      <TitleCards title={"Top Picks for You"}/>
+      <TitleCards title={"Blockbuster Movies"} category = {"top_rated"}/>
+      <TitleCards title={"Only On Netflix"} category = {"popular"}/>
+      <TitleCards title={"Upcoming"} category = {"upcoming"}/>
+      <TitleCards title={"Top Picks for You"} category = {"now_playing"}/>
       </div>
       <Footer/>
     </div>

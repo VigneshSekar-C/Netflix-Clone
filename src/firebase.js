bddec6ba274310240cc4a9e529d18b4654a67b2b
VigneshSekar-C <vigneshsekar1006@gmail.com>
2025-03-08 22:27:43 +0530

    Made some changes

---
+++ src/firebase.js
@@ -0,0 +1,53 @@
+
+import { initializeApp } from "firebase/app";
+import {    
+    createUserWithEmailAndPassword, 
+    getAuth,
+    signInWithEmailAndPassword,    
+    signOut
+        } from "firebase/auth";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA9KhNTT_bjuvRAu6P3mEl7hbl8_Abp4Wo",
+  authDomain: "netflix-clone-8fbaa.firebaseapp.com",
+  projectId: "netflix-clone-8fbaa",
+  storageBucket: "netflix-clone-8fbaa.firebasestorage.app",
+  messagingSenderId: "443815081578",
+  appId: "1:443815081578:web:6fdb138fccdf2c9b9dbc13"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signup = async (name, email, password) => {
+    try {
+        const res = await createUserWithEmailAndPassword(auth, email, password);
+        const user = res.user;
+        await addDoc (collection(db, "user"), {
+            uid: user.uid,
+            name,
+            authProvider: "local",
+            email,
+        })
+    } catch (error) {
+        console.log(error);
+        alert(error);
+    }
+}
+
+const login = async (email, password) => {
+    try {
+        await signInWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+        console.log(error);
+        alert(error);
+    }
+}
+
+const logout = async () => {
+    signOut(auth);
+}
+
+export { auth, db, signup, login, logout };
--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,17 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Player.css'
 import back_arrow_icon from '../../assets/back_arrow_icon.png'
+import { useNavigate, useParams } from 'react-router-dom'
 
 
 const Player = () => {
+
+  const {id} = useParams();
+  const navigate  = useNavigate();
+
+  const [apiData, setApiData] = useState({
+    name:"",
+    key:"",
+    published_at:"",
+    typeof:""
+  })
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYmI0Y2JhYWNhOWUzMWU2NTVmMTc0YTNhMWM5N2JkNyIsIm5iZiI6MTc0MDY3NDg4MS42Nywic3ViIjoiNjdjMDk3NDFhNmU1NTExMTlhMzVhNmUzIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.iaPGlZEfm_NgKc2_7Yk_uMPjIgiZfoH16fZeFKIDh9Q'
+    }
+  };
+
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+    .then(response => response.json())
+    .then(response => setApiData(response.results[0]))
+    .catch(err => console.error(err));
+  }, [])
+  
+  
   return (
     <div className="player">
-      <img src={back_arrow_icon} alt="" />
-      <iframe width='90%' height='90%' src='https://www.youtube.com/embed/tsu0Rw3Nqi8' title='trailer' frameBorder='0' allowFullScreen></iframe>
+      <img src={back_arrow_icon} alt="" onClick={()=>{navigate(-2)}}/>
+      <iframe width='90%' height='90%' src={`https://www.youtube.com/embed/${apiData.key}`} 
+      title='trailer' frameBorder='0' allowFullScreen></iframe>
       <div className="player-info">
-        <p>Published Date</p>
-        <p>Name</p>
-        <p>Type</p>
+        <p>{apiData.published_at.slice(0,10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
       </div>
     </div>
   )

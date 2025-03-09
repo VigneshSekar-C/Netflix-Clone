7cd34f094b770cd5e6f3f16ebe8b5cea45f90ca4
VigneshSekar-C <vigneshsekar1006@gmail.com>
2025-03-09 14:54:44 +0530

    Completed Netflix Clone

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Login from './pages/Login/Login'
 import Player from './pages/Player/Player'
 import { onAuthStateChanged } from 'firebase/auth'
 import { auth } from './firebase'
+import { ToastContainer, toast } from 'react-toastify';
 
 const App = () => {
 
@@ -18,11 +19,13 @@ const App = () => {
       } else {
         console.log("Logged Out");
         navigate("/login");
+
       }
     })
   },[])
   return (
     <div>
+      <ToastContainer theme='dark' />
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/login' element={<Login/>}/>
